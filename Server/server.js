5515da59bbdfaebfb2408337889d04de727c2660
Nathanael Wa Mwenze <11463376+nmwenz90@users.noreply.github.com>
2020-06-06 10:44:00 -0700

    server js file updated

---
+++ Server/server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+// const path = require('path')
+
+const app = express()
+
+const PORT = 3000
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+
+app.listen(PORT, () => console.log(`now connected to port ${PORT}`))
+
+module.exports = app
+
+
+
