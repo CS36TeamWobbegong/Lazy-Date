ea15b238d97c77891b3503e08f5607fc00f8cc9a
Grace Spletzer <gracespletzer05@gmail.com>
2020-06-07 01:55:50 -0700

    seat geeks controller working

--- server/controllers/maincontroller.js
+++ server/controllers/maincontroller.js
@@ -4,7 +4,6 @@ const fetch = require('node-fetch')
 const maincontroller = {};
 
 maincontroller.getYelpRecs = (req, res, next) => {
-  //extract keywords from from the req.body? or req.params?
   //declare a variable to hold string of categories
   const urlString = req.body.categories;
   //declare a variable to hold string/number of zip code
@@ -15,17 +14,14 @@ maincontroller.getYelpRecs = (req, res, next) => {
   //make API call with apiCall variable as first parameter, and second parameter as object with header auth key and stringify body command
   fetch(apiCall, 
     {
-    //method: "GET",
     headers: {
        //yelpApiKey in header
       "Authorization": "Bearer osYDEYEU59-jf1Ng4VaMuSOnuXA46CO9qQWS1i1BQcsnfDjRjZjQ2owOdtq_i623ntNkIXZqXbr9JWuYHbUjD5mY2N6f3H7vhk77v5itlJ75Tin70sN38cN71fvaXnYx"
     },
-      //body: json stringify
-    //body: JSON.stringify(data) 
+      
     })
     //then process results from API call into json
     .then(data => {
-      console.log('made it to first then');
       return data.json()})
     //then assign results to res.locals.results
     .then(results => {
@@ -37,12 +33,13 @@ maincontroller.getYelpRecs = (req, res, next) => {
       const {name, image_url, location, display_phone} = results.businesses[i];
       //push the deconstructed object to the empty array 
       //ran join on the location.display_address to turn it into a string
-      newArr.push({name: name, image_url: image_url, location: location.display_address.join(', '), display_phone: display_phone})
+      newArr.push({name: name, image_url: image_url, location: location.display_address.join(', '), contact: display_phone})
       }
       //add newArr to the res locals results
       res.locals.results = newArr;
       //return results to router
       return next()})
+      //add a catch for errors
     .catch(err => {
       return next({
         log: "Error in getYelpRecs" + err,
@@ -51,7 +48,41 @@ maincontroller.getYelpRecs = (req, res, next) => {
 };
 
 maincontroller.getSGRecs = (req, res, next)=> {
+  //declare a variable to hold string of categories
+  const urlString = req.body.categories;
+  //declare a variable to hold string/number of zip code
+  const urlLocation = req.body.location;
+  //use object literals to build api call string with result limit of 5
+  const apiURL = `https://api.seatgeek.com/2/events?per_page=5&postal_code=${urlLocation}&q=${urlString}`
+  fetch(apiURL, 
+    {
+    headers: {
+       //yelpApiKey in header
+       'Authorization': 'Basic T1RNek1EazFNbnd4TlRreE5EQTNPVGd5TGpVMTo='
+      },
+    })
+    //then process results from API call into json
+    .then(data => {
 
+      return data.json()})
+    //then assign results to res.locals.results
+    .then(results => {
+      const formattedResults = [];
+      for (let i = 0; i < results.events.length; i++){
+        formattedResults.push({
+          name: results.events[i].title,
+          venue: results.events[i].venue.name,
+          location: results.events[i].venue.address + ', ' + results.events[i].venue.extended_address,
+          contact: results.events[i].url
+        })
+      }
+      res.locals.results = formattedResults;
+      return next();
+    })
+    .catch(err => {
+      return next({ log: "Error in seatgeek request: ", err });
+    })
+  
 }
 
 module.exports = maincontroller;
