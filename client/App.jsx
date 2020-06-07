22d02ef5ae16ac4df897d9f3ce69745fd7c4065e
Nathanael Wa Mwenze <11463376+nmwenz90@users.noreply.github.com>
2020-06-07 15:11:53 -0700

    update to categories container server js and routes js

--- client/App.jsx
+++ client/App.jsx
@@ -10,23 +10,55 @@ class App extends Component {
             results: [],
             optionCategory: ["games", "arts and culture", "outdoor", "culinary"],
             optionDescription: ["fun", "adventure", "relaxation", "entertainment"],
-            optionLocation:[
-                {city: "Santa Monica", zipcode: 90401},
-                {city: "West Hollywood", zipcode: 90069},
-                {city: "Studio City", zipcode: 91604},
-                {city: "DTLA", zipcode: 90017}
+            optionLocation: [
+                { city: "Santa Monica", zipcode: 90401 },
+                { city: "West Hollywood", zipcode: 90069 },
+                { city: "Studio City", zipcode: 91604 },
+                { city: "DTLA", zipcode: 90017 }
             ]
         }
+        this.handleSubmit = this.handleSubmit.bind(this)
     }
+
+    handleSubmit(e) {
+        console.log('button clicked!')
+        e.preventDefault()
+        const requestBody = {
+            categories: e.target.optionDescription.value,
+            location: e.target.optionLocation.value,
+        }
+        console.log(requestBody);
+        fetch('/api/search/yelp', {
+            method: 'POST',
+            body: JSON.stringify(requestBody),
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        })
+            .then((data) => {
+                console.log(data, '<---- DATA IN FIRST THEN')
+                return data.json();
+            })
+            .then((jsonData) => {
+                console.log(jsonData, '<---- DATA IN SECOND THEN')
+                this.setState({
+                    results: jsonData
+                })
+            })
+            .catch((err) => {
+                console.log("ERROR IN CATCH OF ADD RESULTS", err)
+            })
+    }
+
     render() {
         return (
             <div>
                 <h1 className="title">Better Together</h1>
-                <CategoriesContainer/>
-                <ResultsContainer/>
+                <CategoriesContainer handleSubmit={this.handleSubmit} optionLocation={this.state.optionLocation} optionDescription={this.state.optionDescription} />
+                <ResultsContainer />
             </div>
         )
-    }    
+    }
 }
 
 export default App;
--- client/containers/CategoriesContainer.jsx
+++ client/containers/CategoriesContainer.jsx
@@ -1,23 +1,46 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class CategoriesContainer extends Component {
-    constructor(props){
+    constructor(props) {
         super(props)
     }
 
     render() {
-        const categories = ['concert', 'sports', 'hike', 'beach', 'skydive'];
-        const keywords = [];
-        for (let i = 0; i < categories.length; i++) {
-            keywords.push(<button key={i}>{categories[i]}</button>);
+        // const categories = ['concert', 'sports', 'hike', 'beach', 'skydive'];
+        //optionDescription is from state
+        const optionDescription = this.props.optionDescription
+        const descriptionArray = [];
+        for (let i = 0; i < optionDescription.length; i++) {
+            descriptionArray.push(<label key={i}>{optionDescription[i]}
+                <input type="radio" name="optionDescription" value={optionDescription[i]} id={optionDescription[i]} key={i} />
+            </label>);
         }
+        //this.props.optionLocation
+        const optionLocation = this.props.optionLocation;
+        const locationArray = [];
+        for (let i = 0; i < optionLocation.length; i++) {
+            locationArray.push(<label key={i}>{optionLocation[i].city}
+                <input type="radio" name="optionLocation" value={optionLocation[i].city} id={optionDescription[i].city} key={i} />
+            </label>)
+        }
+
         return (
             <div className='box'>
-                <h2>Categories</h2>
-                <p>Choose what type of date you want</p>
-                <div>{keywords}</div>
-                
-                <button>Submit</button>
+
+
+                <form onSubmit={this.props.handleSubmit}>
+                    <label>
+                        <h2>Categories</h2>
+                        <p>Choose what type of date you want</p>
+                    </label>
+                    {descriptionArray}
+                    <label>
+                        <h2>Neighborhood</h2>
+                        <p>Choose your neighborhood</p>
+                    </label>
+                    {locationArray}
+                    <input type="submit" value="submit" />
+                </form>
             </div>
         )
     }
