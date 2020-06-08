70220f3639bdcccc0cff1ae3d192c6771ba94f68
Sean Smith <smithsean@MacBook-Pro.local>
2020-06-08 14:54:18 -0700

    some verification working

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const nodeFetch = require('node-fetch')
 //declare a router for api requests
 const apiRouter = require('./routes/api.js')
 const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser')
 require('dotenv').config();
 
 const URI = process.env.MONGO_URI
@@ -16,7 +17,7 @@ const URI = process.env.MONGO_URI
 mongoose.connect(URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
-  })
+})
     .then(() => console.log('CONNECTED TO MONGO DB'))
     .catch((err) => console.log('ERROR CONNECTING TO DB: ', err));
 
@@ -44,8 +45,14 @@ const PORT = 3000;
 
 //set-up request processing
 app.use(express.json());
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
+    console.log('req on initial get ->>>>>>>>', req)
+    console.log(req.cookies, ' <====== req.cookies in initial get')
+    if (req.cookies.test === 'practice') {
+        return res.redirect('/home')
+    }
     res.redirect('/signup.html');
 })
 //static files
@@ -55,41 +62,54 @@ app.use('/build', express.static(path.resolve(__dirname, '../build')))
 //     res.sendFile(path.join(__dirname, '../client/signup.html'))
 // })
 
-app.use('/home', express.static(path.resolve(__dirname, '../client')))
+function verifyUser(req, res, next) {
+    if (req.cookies.ambiguous) {
+        return next();
+    }
+    res.redirect('/signup.html')
+}
+
+app.use('/home', verifyUser, express.static(path.resolve(__dirname, '../client')))
 //send /api requests to api router
 app.use('/api', apiRouter)
 
-app.post('/signup',(req, res) => {
+app.post('/signup', (req, res) => {
     console.log('do i show up')
     console.log(req.body)
-    // create user in database useing req.body for username and password with a create
+    // create user in database using req.body for username and password with a create
     const username = req.body.username;
     const password = req.body.password;
-    Users.create( {username, password}, (err, data) => {
-        if (err){
+    Users.create({ username, password }, (err, data) => {
+        if (err) {
             console.log("ERROR IN CREATE USER")
-            return res.status(500).json('Error, check server log for details')
+            res.redirect('/signup.html')
+        } else {
+            console.log('SUCCESSFULLY ADDED USER')
+            console.log(data)
+            res.cookie('ambiguous', `${data._id}`, { httpOnly: true, maxAge: 1000000 })
+            res.redirect('/home')
         }
-        console.log('SUCCESSFULLY ADDED USER')
-        res.redirect('/home')
-    } )
+    })
+
 })
 
 app.post('/login', (req, res) => {
-    console.log(req.body, '<---- req.body')
     const username = req.body.username;
     const password = req.body.password;
-    Users.find({username: username}, (err, result) => {
-        if (err){
+    Users.find({ username: username }, (err, result) => {
+        if (err) {
             console.log('ERROR IN FIND USER');
             res.status(500).json('Error, check server log for details')
         }
-        if (result.length === 0 || result[0].password !== password){
+        if (result.length === 0 || result[0].password !== password) {
             console.log('no results found with that username')
             // maybe just have something pop up on the page like yo go to the signup page or try again
+
             return res.redirect('/login.html');
         }
         console.log('SUCCESSFULLY LOGGED IN');
+        console.log(result, '<--------------------------result')
+        res.cookie('ambiguous', `${result[0]._id}`, { httpOnly: true, maxAge: 1000000 })
         res.redirect('/home');
     })
 })
